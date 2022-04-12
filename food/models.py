d75e6b575fc30b02f494d58b425dfd267f478484
Anshul Kanwar <4n5hu1@protonmail.com>
2022-04-12 21:11:10 +0530

    Added basic API For featching Food Items

---
+++ food/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Food(models.Model):
+    name = models.CharField(max_length=100)
+    carbohydrates = models.DecimalField(max_digits=5, decimal_places=2, blank=True)
+    protien = models.DecimalField(max_digits=5, decimal_places=2, blank=True)
+    fats = models.DecimalField(max_digits=5, decimal_places=2, blank=True)
+
+    def __str__(self) -> str:
+        return self.name
