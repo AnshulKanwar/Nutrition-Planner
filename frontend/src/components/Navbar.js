84fc3e2364eb08e4f26b2e9ff1238ce488e7cc30
Anshul Kanwar <4n5hu1@protonmail.com>
2022-04-19 21:19:58 +0530

    Added persisting Login

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,7 +4,7 @@ import { useContext } from "react";
 import AuthContext from "../context/AuthContext";
 
 const Navbar = () => {
-  const { user } = useContext(AuthContext);
+  const { user, logoutUser } = useContext(AuthContext);
 
   return (
     <div className="pt-10 pb-20">
@@ -14,10 +14,17 @@ const Navbar = () => {
           <h1 className="text-3xl font-bold">Nutrition Planner</h1>
         </div>
         {user && (
-          <button className="flex items-center gap-x-2 font-bold hover:text-fuchsia-500">
-            <BiUser className="text-xl" />
-            <span>{user.first_name.charAt(0).toUpperCase() + user.first_name.slice(1)}</span>
-          </button>
+          <div className="flex gap-x-2">
+            <button className="flex items-center gap-x-2 font-bold hover:text-fuchsia-500">
+              <BiUser className="text-xl" />
+              <span>
+                {user.first_name.charAt(0).toUpperCase() +
+                  user.first_name.slice(1)}
+              </span>
+            </button>
+            <span className="font-light text-2xl">|</span>
+            <button onClick={logoutUser} className="hover:text-black hover:underline">Logout</button>
+          </div>
         )}
       </div>
     </div>
--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,4 +1,4 @@
-import { useState, createContext } from "react";
+import { useState, createContext, useEffect } from "react";
 import axios from "axios";
 import jwtDecode from "jwt-decode";
 import { useNavigate } from "react-router-dom";
@@ -8,34 +8,48 @@ const AuthContext = createContext();
 export default AuthContext;
 
 export const AuthProvider = ({ children }) => {
+  const storedAuthTokens = localStorage.getItem("authTokens");
 
-  const [authTokens, setAuthTokens] = useState(null);
-  const [user, setUser] = useState(null);
+  const [authTokens, setAuthTokens] = useState(
+    storedAuthTokens ? JSON.parse(storedAuthTokens) : null
+  );
+  const [user, setUser] = useState(
+    storedAuthTokens ? jwtDecode(storedAuthTokens) : null
+  );
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const loginUser = (e) => {
     e.preventDefault();
     axios
       .post("/api/token/", {
-        email: "anshul@gmail.com",
-        password: "test123",
+        email: e.target.username.value,
+        password: e.target.password.value,
       })
       .then((res) => {
-        setAuthTokens(res.data)
-        setUser(jwtDecode(res.data.access))
-        navigate('/')
+        setAuthTokens(res.data);
+        setUser(jwtDecode(res.data.access));
+        localStorage.setItem('authTokens', JSON.stringify(res.data))
+        navigate("/");
       })
       .catch((err) => {
         //TODO: Add Error Handling
-        alert(err)
+        alert(err);
       });
   };
+
+  const logoutUser = () => {
+    setAuthTokens(null)
+    setUser(null)
+    localStorage.removeItem('authTokens')
+    navigate('/login')
+  }
 
   const contextData = {
     authTokens,
     user,
     loginUser,
+    logoutUser,
   };
 
   return (
